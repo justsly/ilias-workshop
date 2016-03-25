72c1efc5a16c104b2d6c3fa5fba917ec3c32a9e3
justsly <b.brixner@t-online.de>
2016-03-26 00:42:08 +0100

    callbacks and soap solution send

--- node/app.js
+++ node/app.js
@@ -7,25 +7,29 @@ var WorkshopModule = (function () {
 	var level_list = [];
 
 	return {
-		Level: function (lkey, lvalue) {
-			this.lkey = lkey;
+		Level: function (lkey, lvalue, qid, answer) {
 			this.lvalue = lvalue;
+			this.qid = qid;
+			this.answer = answer;
 		},
 		addNewLevel : function (litem){
 			level_list.push(litem);
 		},
-		getLevelById: function (lkey) {
+		getLevelById: function (lkey, cb) {
 			for (var i = 0; i < level_list.length; i++) {
 				if (level_list[i].lkey === lkey) {
-					return level_list[i].lvalue;
+					return ((typeof(cb) === 'function') ? cb(null, level_list[i]) : level_list[i]);
 				}
 			}
-			return null;
+			return ((typeof(cb) === 'function') ? cb(null, null) : null);
 		},
 		//Define object DockerContainer
-		DockerContainer: function (docker_h, docker_p) {
+		DockerContainer: function (docker_h, docker_p, active_id) {
 			this.docker_hash = docker_h;
 			this.docker_port = docker_p;
+			this.active_id = active_id;
+			this.uid = uid;
+			this.lid = lid;
 		},
 		//Push new DockerContainer object to list
 		addNewContainer: function (dc) {
@@ -42,34 +46,39 @@ var WorkshopModule = (function () {
 			return false;
 		},
 		//Returns DockerContainer object from list if matches given hash
-		findContainerByHash: function (docker_hash) {
+		findContainerByHash: function (docker_hash, cb) {
 			for (var i = 0; i < container_list.length; i++) {
 				if (container_list[i].docker_hash === docker_hash) {
-					return container_list[i].docker_port;
+					return ((typeof(cb) === 'function') ? cb(null, container_list[i]) : container_list[i]);
 				}
 			}
-			return null;
+			return ((typeof(cb) === 'function') ? cb(null, null) : null);
 		},
 		//Create DockerContainer and redirect User to this Instance
-		createDockerContainer: function (lvalue, active_id, res) {
-			var cmd = 'docker run -dit -p 0:80 sclyther/' + lvalue;
-			exec(cmd, function (error, stdout, stderr) {
-				if (!error) {
-					docker_hash = stdout.match(/[0-9a-f]+/)[0];
-					exec('docker port ' + docker_hash, function (error, stdout, stderr) {
+		createDockerContainer: function (lid, active_id, uid, res) {
+			WorkshopModule.getLevelById(lid, function(err, litem) {
+				if(level) {
+					console.log('level exists: ' + litem.lvalue);
+					var cmd = 'docker run -dit -p 0:80 sclyther/' + litem.lvalue;
+					exec(cmd, function (error, stdout) {
 						if (!error) {
-							var docker_port = stdout.match(/\:\d+/)[0];
-							WorkshopModule.addNewContainer(new WorkshopModule.DockerContainer(docker_hash, docker_port));
-							setTimeout(function () {
-								res.writeHead(302, {
-									'Location': 'http://' + srv_ip + '' + docker_port,
-									'Set-Cookie': ['dockerHash=' + docker_hash + '; Path=/;', 'active_id=' + active_id + '; Path=/;']
-								});
-								res.end();
-								setTimeout(function () {
-									WorkshopModule.destroyContainer(docker_hash);
-								}, 1800000);
-							}, 3000);
+							docker_hash = stdout.match(/[0-9a-f]+/)[0];
+							exec('docker port ' + docker_hash, function (error, stdout) {
+								if (!error) {
+									var docker_port = stdout.match(/\:\d+/)[0];
+									WorkshopModule.addNewContainer(new WorkshopModule.DockerContainer(docker_hash, docker_port, active_id, uid, lid));
+									setTimeout(function () {
+										res.writeHead(302, {
+											'Location': 'http://' + srv_ip + '' + docker_port,
+											'Set-Cookie': ['dockerHash=' + docker_hash + '; Path=/;']
+										});
+										res.end();
+										setTimeout(function () {
+											WorkshopModule.destroyContainer(docker_hash);
+										}, 1800000);
+									}, 3000);
+								}
+							});
 						}
 					});
 				} else {
@@ -83,15 +92,16 @@ var WorkshopModule = (function () {
 			exec(cmd);
 			WorkshopModule.removeContainerByHash(docker_hash);
 		},
-		checkExistingContainer: function (req, res) {
+		checkExistingContainer: function (req, res, cb) {
 			if (req.cookies) var docker_hash = req.cookies['dockerHash'];
-			var docker_port = WorkshopModule.findContainerByHash(docker_hash);
-			if (docker_port) {
-				WorkshopModule.redirectToPort(docker_port, res);
-				return true;
-			} else {
-				return false;
-			}
+			WorkshopModule.findContainerByHash(docker_hash, function(err, citem) {
+				if (citem) {
+					WorkshopModule.redirectToPort(citem.docker_port, res);
+					return ((typeof(cb) === 'function') ? cb(null, true) : true);
+				} else {
+					return ((typeof(cb) === 'function') ? cb(null, false) : false);
+				}
+			});
 		},
 		redirectToPort: function (docker_port, res) {
 			res.writeHead(302, {
@@ -99,11 +109,10 @@ var WorkshopModule = (function () {
 			});
 			res.end();
 		},
-        //@todo fix this with correct soap call
+        //SOAP Call to check if uid is valid in ILIAS
         checkValidSid : function (uid, cb) {
-			var url = 'http://localhost/webservice/soap/server.php?wsdl';
 			var args = {sid: uid + '::ilias'};
-			soap.createClient(url, function(err, client) {
+			soap.createClient(wsdl_url, function(err, client) {
 				client.getUserIdBySid(args, function(err, result) {
 					if(result.usr_id){
 						return ((typeof(cb) === 'function') ? cb(null, true) : true);
@@ -112,7 +121,20 @@ var WorkshopModule = (function () {
 					}
 				});
 			}.bind(this));
-        }
+        },
+		sendSolutionToILIAS : function (answer, uid, aid, qid, cb) {
+			var sol = "<values><value>" + answer + "</value><value></value><points>5</points></values>";
+			var args = {sid: uid + '::ilias', active_id: aid, question_id: qid, pass: 0, solution: sol};
+			soap.createClient(wsdl_url, function(err, client) {
+				client.saveQuestionSolution(args, function(err, result) {
+					if(result.statusCode == 200) {
+						return ((typeof(cb) === 'function') ? cb(null, true) : true);
+					} else {
+						return ((typeof(cb) === 'function') ? cb(null, false) : false);
+					}
+				})
+			}.bind(this));
+		}
 	}
 })();
 
@@ -132,6 +154,9 @@ if (!inDebug) {
 
 
 app.use(cookieParser());
+
+//WSDL URL
+const wsdl_url = "http://localhost/webservice/soap/server.php?wsdl";
 
 // Define Server IP and Port
 const srv_ip = '192.168.56.101';
@@ -153,51 +178,65 @@ var allowCrossDomain = function(req, res, next) {
 app.use(allowCrossDomain);
 
 app.use(function(req, res, next){
-	var exists = WorkshopModule.checkExistingContainer(req, res);
-	if(!exists) next();
+	WorkshopModule.checkExistingContainer(req, res, function(err, exists) {
+		if(!exists) next();
+	});
 });
 
 //Define Routing for the Websecurity Levels
 app.get('/container/create/:level_id/active_id/:active_id/uid/:uid', function(req, res){
-	var level = WorkshopModule.getLevelById(req.params.level_id);
-	console.log("level exists: " + level);
 	WorkshopModule.checkValidSid(req.params.uid, function(err, isvalid) {
 		console.log("valid uid: " + isvalid);
-		if(level && isvalid) {
-			WorkshopModule.createDockerContainer(level, req.params.active_id, res);
+		if(isvalid) {
+			WorkshopModule.createDockerContainer(req.params.level_id, req.params.active_id, req.params.uid, res);
 		} else {
 			return res.status(401).send({success: false, error: 'denying docker creation because of missing sid / level'});
 		}
 	});
-
 });
 
 // Return to ILIAS with complete flag
 app.get('/container/:docker_hash/complete', function(req, res){
-	var container_item = WorkshopModule.findContainerByHash(req.params.docker_hash);
-	if(container_item){
-		res.status(200).send({success:true, message: 'User: ' + container_item.uid + 'hat das Level erfolgreich beendet!'})
-	} else {
-		res.status(500).send({success:false, error: 'container not found!'});
-	}
+	WorkshopModule.findContainerByHash(req.params.docker_hash, function(err, citem) {
+		if(citem){
+			WorkshopModule.getLevelById(citem.lid, function(err, litem) {
+				WorkshopModule.sendSolutionToILIAS(litem.answer, citem.uid, citem.active_id, citem.qid, function(err, result) {
+					if(result) {
+						res.status(200).send({success:true, message: 'User: ' + citem.active_id + 'hat das Level: ' + litem.lvalue + ' erfolgreich beendet!'});
+					} else {
+						res.status(500).send({success:false, message: 'Internal Error. Could not send solution to ILIAS.'});
+					}
+				});
+			});
+		} else {
+			res.status(404).send({success:false, error: 'container not found!'});
+		}
+		res.end();
+	});
 });
 
 //Define Routing for Container Flush
 app.delete('/container/:docker_hash/end', function(req, res){
-	WorkshopModule.destroyContainer(req.params.docker_hash);
-	res.status(200).send({success:true});
-	res.end();
+	WorkshopModule.findContainerByHash(req.params.docker_hash, function(err, citem) {
+		if(citem){
+			WorkshopModule.destroyContainer(req.params.docker_hash);
+			res.status(200).send({success:true});
+		} else {
+			res.status(404).send({success:false, error: 'container not found!'});
+		}
+		res.end();
+	});
 });
 
 //Default Catch for wrong URLs sends 404
 app.get('*', function(req, res){
-	res.status(404).send({success:false, error:"Container not found!"});
+	res.status(404).send({success:false, error:"Ressource not found!"});
 	res.end();
 });
 
 
 //Define Levels
-WorkshopModule.addNewLevel(new WorkshopModule.Level('base', 'bb_ws_base_image'));
-WorkshopModule.addNewLevel(new WorkshopModule.Level('cmdi01', 'bb_ws_cmdi_01'));
-WorkshopModule.addNewLevel(new WorkshopModule.Level('ping', 'ilias_cmdi01'));
+WorkshopModule.addNewLevel(new WorkshopModule.Level('base', 'bb_ws_base_image', 1, 'base1'));
+WorkshopModule.addNewLevel(new WorkshopModule.Level('cmdi01', 'bb_ws_cmdi_01', 1, 'cmdi1'));
+WorkshopModule.addNewLevel(new WorkshopModule.Level('ping', 'ilias_cmdi01', 2, 'test456'));
 
