a272cc29b4cb31161c30b71f1349af671514819a
justsly <b.brixner@t-online.de>
2016-04-06 12:49:18 +0200

    testing new redirect method

--- node/app.js
+++ node/app.js
@@ -305,12 +305,12 @@ var WorkshopModule = (function () {
 						"<link href='../css/style.css' rel='stylesheet'>" +
 					"</head>" +
 					"<body>" +
-						"<p class='zentriert'>Please wait for Container is build. You will be redirected to your Container in " + delay_seconds + " seconds</p>" +
+						"<p>Please wait for Container is build. You will be redirected to your Container in " + delay_seconds + " seconds</p>" +
 						"<div class='spinner'>" +
 							"<div class='cube1'></div>" +
 							"<div class='cube2'></div>" +
 						"</div>" +
-						"<p class='zentriert'>If your redirect does not work please click <a href='" + url + "'>here</a></p>" +
+						"<p>If your redirect does not work please click <a href='" + url + "'>here</a></p>" +
 					"</body>" +
 					"</html>";
 
