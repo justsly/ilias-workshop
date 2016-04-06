7c0a3ef9a2b30690d2b0238646970aa81dd388c2
Björn Brixner <b.brixner@t-online.de>
2016-04-06 18:31:58 +0200

    try to add entrypoint to cmdi01

--- levels/cmdi01/setup.sh
+++ levels/cmdi01/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-mv /var/www/html/secret.php /var/www/html/secret_$(date|md5sum|cut -c 1-16).php
-/root/secret_generator.sh
+mv /var/www/html/secret.php /var/www/html/thats_the_secret_$(date|md5sum|cut -c 1-16).php
+/usr/sbin/apache2ctl start
+secret=$1
+echo '<?php' > /var/www/html/dc_secret.php
+echo '$dc_secret="'"$secret"'";' >> /var/www/html/dc_secret.php
+echo '?>' >> /var/www/html/dc_secret.php
