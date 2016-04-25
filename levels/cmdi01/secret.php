4e40fc92c0f6d15a0451daf023fb22221ebe17a0
justsly <b.brixner@t-online.de>
2016-04-25 20:20:35 +0200

    fixed the cat bug

--- levels/cmdi01/secret.php
+++ levels/cmdi01/secret.php
@@ -26,7 +26,7 @@ echo "</script>";
 
 <script>
 	$("#complete_level").click(function(){
-	    if(typeof srv_ip === "undefined"){
+	    if(typeof srv_ip !== "undefined"){
 	        $.ajax({
 		        url: 'http://' + srv_ip + srv_port + '/container/' + getCookie('dockerHash') + '/complete/secret/' + dc_secret,
 		        type: 'GET',
